0febc6ccf9b947999925c97a25f9b7ea486757c7
Thao Ma <MaThao@gmail.com>
2021-01-05 16:40:30 -0700

    Edit let to const and changed all the anonymous functions to arrow functions

--- test/user-test.js
+++ test/user-test.js
@@ -3,10 +3,10 @@ import { expect } from 'chai';
 import User from '../src/user';
 import data from '../src/data/users-data';
 
-describe('User', function() {
-  let user;
-  let userInfo;
-  let recipe;
+describe('User', () => {
+  const user;
+  const userInfo;
+  const recipe;
 
   beforeEach(() => {
     userInfo = data[0];
@@ -15,46 +15,46 @@ describe('User', function() {
     recipe = {name: 'Chicken Parm', type: ['italian', 'dinner']};
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(User).to.be.a('function');
   });
 
-  it('should initialize with an id', function() {
+  it('should initialize with an id', () => {
     expect(user.id).to.eq(1);
   });
 
-  it('should initialize with a name', function() {
+  it('should initialize with a name', () => {
     expect(user.name).to.eq('Saige O\'Kon');
   });
 
-  it('should initialize with a pantry', function() {
+  it('should initialize with a pantry', () => {
     expect(user.pantry[0].ingredient).to.eq(11477);
   });
 
-  it('should initialize with an empty favoriteRecipes array', function() {
+  it('should initialize with an empty favoriteRecipes array', () => {
     expect(user.favoriteRecipes).to.deep.equal([]);
   });
 
-  it('should initialize with an empty recipesToCook array', function() {
+  it('should initialize with an empty recipesToCook array', () => {
     expect(user.recipesToCook).to.deep.equal([]);
   });
 
-  it('should be able to save a recipe to favoriteRecipes', function() {
+  it('should be able to save a recipe to favoriteRecipes', () => {
     user.saveRecipe(recipe);
     expect(user.favoriteRecipes[0].name).to.equal('Chicken Parm');
   });
 
-  it('should be able to decide to cook a recipe', function() {
+  it('should be able to decide to cook a recipe', () => {
     user.decideToCook(recipe);
     expect(user.recipesToCook[0].name).to.equal('Chicken Parm');
   });
 
-  it('should be able to filter recipes by type', function() {
+  it('should be able to filter recipes by type', () => {
     user.saveRecipe(recipe);
     expect(user.filterRecipes('italian')).to.deep.equal([recipe]);
   });
 
-  it('should be able to search recipes by name', function() {
+  it('should be able to search recipes by name', () => {
     user.saveRecipe(recipe);
     expect(user.searchForRecipe('Chicken Parm')).to.deep.equal([recipe]);
   });
