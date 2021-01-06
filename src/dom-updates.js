1dfee967ed9215a60e7530602d1036a5ac246229
srslie <aliceruppert@gmail.com>
2021-01-05 20:36:44 -0700

    Add displayRecipeCard method

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -8,3 +8,20 @@ const domUpdates = {
         document.querySelector(".banner-image").insertAdjacentHTML("afterbegin",
             welcomeMsg);
     },
+    
+
+    displayRecipeCard(recipeInfo, shortRecipeName) {
+        const cardHtml = 
+            `<div class="recipe-card" id=${recipeInfo.id}>
+                <h3 maxlength="40">${shortRecipeName}</h3>
+                <div class="card-photo-container">
+                    <img src=${recipeInfo.image} class="card-photo-preview" alt="${recipeInfo.name} recipe" title="${recipeInfo.name} recipe">
+                    <div class="text">
+                        <div>Click for Instructions</div>
+                    </div>
+                </div>
+                <h4>${recipeInfo.tags[0]}</h4>
+                    <img src="../images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
+            </div>`;
+        main.insertAdjacentHTML("beforeend", cardHtml);
+    },
