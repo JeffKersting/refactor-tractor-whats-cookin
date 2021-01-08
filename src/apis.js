1cd871fc3ce347938d1de7b7ac5db3fea7356d9b
srslie <aliceruppert@gmail.com>
2021-01-08 14:57:58 -0700

    Change spacing

--- src/apis.js
+++ src/apis.js
@@ -1,26 +1,26 @@
 // use get to grab whatever dataType (users/ingredients/recipes)
 export function getData(dataType, saveVar) {
-    fetch(`http://localhost:3001/api/v1/${dataType}`)
-        .then(response => response.json())
-        .then(dataArray => dataArray
-            .forEach(dataObject => saveVar.push(dataObject)))
-        .catch(error => alert(`Sorry, there is an error: ${error}`))
+  fetch(`http://localhost:3001/api/v1/${dataType}`)
+    .then(response => response.json())
+    .then(dataArray => dataArray
+      .forEach(dataObject => saveVar.push(dataObject)))
+    .catch(error => alert(`Sorry, there is an error: ${error}`))
 }
 
 // use post to modify ingredients 
 export function postData(userId, ingredientId, amount) {
-    const settings = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 
-            userID: userId, 
-            ingredientID: ingredientId, 
-            ingredientModification: amount
-        })
-    }
-    fetch('http://localhost:3001/api/v1/users', settings)
-        .then(response => response.json())
-        .catch(error => alert(`Sorry, there is an error: ${error}`))
+  const settings = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      userID: userId, 
+      ingredientID: ingredientId, 
+      ingredientModification: amount
+    })
+  }
+  fetch('http://localhost:3001/api/v1/users', settings)
+    .then(response => response.json())
+    .catch(error => alert(`Sorry, there is an error: ${error}`))
 }
