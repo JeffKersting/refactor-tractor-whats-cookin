b60edd9116355f0fff92a0668778e90d9748e477
Allison Dietz <dietz.allison@outlook.com>
2021-01-09 17:36:03 -0700

    remove console.logs Ingredient.js

--- src/ingredient.js
+++ src/ingredient.js
@@ -1,11 +1,6 @@
 class Ingredient {
   constructor(ingredient, recipeIngredientData) {
-    // console.log('recipeIngredientData.name >>>', recipeIngredientData.name);
-    // console.log('ingredient.name >>>', ingredient.name);
-
-    
     this.id = ingredient.id,
-    // console.log('ingredient.id >>>', ingredient.id);
     this.name = recipeIngredientData.name,
     this.estimatedCostInCents = ingredient.estimatedCostInCents,
     this.quantity = recipeIngredientData.quantity
