91abf7237ff2492d9d88f000a4f43fd6ae492a75
srslie <aliceruppert@gmail.com>
2021-01-10 19:59:59 -0700

    Add checkbox filter, ingredient form func

--- src/scripts.js
+++ src/scripts.js
@@ -1,46 +1,185 @@
 /* eslint-disable max-len */
-import './css/index.scss';
-import {getData, postData} from './apis'
-import domUpdates from './dom-updates';
-
-import users from './data/users-data';
-import recipeData from  './data/recipe-data';
-import ingredientData from './data/ingredient-data';
-
+//IMPORT CSS
 import './css/index.scss';
 import './images/apple-logo.png'
 import './images/search.png'
 import './images/seasoning.png'
 import './images/cookbook.png'
 import './images/pot.png'
-
+//IMPORT CLASSES 
+import {getData, postData} from './apis'
+import domUpdates from './dom-updates';
 import User from './user';
 import Recipe from './recipe';
 
-const fullRecipeInfo = document.querySelector(".recipe-instructions");
-
-let pantryInfo = [];
-let recipes = [];
-let ingredients = [];
 
 window.addEventListener("load", loadPage);
 
 const loginInput = document.querySelector('.user-input');
+
+function addClickEvent(area, func) {
+  document.querySelector(area).addEventListener('click', func)
+}
+
 addClickEvent(".login-btn", login)
-addClickEvent(".show-all-btn", showAllRecipes)
-addClickEvent(".filter-btn", findCheckedBoxes)
-addClickEvent("main", addToMyRecipes)
+// addClickEvent(".show-all-btn", showAllRecipes)
+// addClickEvent(".filter-btn", findCheckedBoxes)
+// addClickEvent("main", addToMyRecipes)
 addClickEvent(".my-pantry-btn", displayMenu)
 addClickEvent(".saved-recipes-btn", domUpdates.showSavedRecipes)
 addClickEvent(".search-btn", domUpdates.searchRecipes)
-addClickEvent(".show-pantry-recipes-btn", findCheckedPantryBoxes)
+// addClickEvent(".show-pantry-recipes-btn", findCheckedPantryBoxes)
 
 // search
 const searchForm = document.querySelector("#search");
 const searchInput = document.querySelector("#search-input");
+
+// search bar button
 searchForm.addEventListener("submit", pressEnterSearch);
 
+function pressEnterSearch(event) {
+  event.preventDefault();
+  domUpdates.searchRecipes();
+}
+
+// load
+  // login page html displays >>>>>> 
+  // get user data to use for login button search >>>>>> 
+  // get all recipes data? >>>>>> 
+  // get all ingredients data? >>>>>> 
+
+let users = [];
+let recipes = [];
+let ingredients = [];
+
+function loadPage() {
+  getData('users', users)
+  getData('recipes', recipes)
+  getData('ingredients', ingredients)
+}
+
+// on login display
+  // create user & display welcome  >>>>>> 
+  // create & hide display pantry >>>>>> 
+  // create & hide display favs  >>>>>> 
+  // create & hide display to cook >>>>>> 
+
+
+let user;
+
+function returnUserId() {
+  const userSearched = users.find(user => user.name === loginInput.value)
+  user = new User(userSearched)
+}
+function createCards() {
+  recipes.forEach(recipe => {
+      const recipeInstance = new Recipe(recipe);
+      recipes.push(recipeInstance);
+      const shortRecipeName = recipeInstance.name;
+      if (recipeInstance.name.length > 40) {
+          shortRecipeName = recipeInstance.name.substring(0, 40) + "...";
+      }
+      domUpdates.displayRecipeCard(recipeInstance, shortRecipeName)
+  });
+}
+
+function login() {
+  returnUserId()
+  createCards()
+  domUpdates.showRecipes(recipes)
+  domUpdates.listTags()
+  domUpdates.displayWelcomeBanner()
+  // MAKE SURE THESE DEFAULT TO HIDDEN CLASS
+  domUpdates.displayPantryInfo(user.pantry.sort((a, b) => a.name.localeCompare(b.name)))
+  //
+}
+
+//FIGURE OUT WHAT TO TOGGLE?
+//showWelcomeBanner & showMyRecipesBanner
+// domUpdates.toggleSelection(".welcome-msg", ".my-recipes-banner")
+
+//toggleMenu
+function displayMenu() {
+  domUpdates.toggleMenu()
+}
+
+function showSavedRecipes() {
+  const unsavedRecipes = recipes.filter(recipe => {
+      return !user.favoriteRecipes.includes(recipe.id);
+  });
+  domUpdates.recipeDisplay(unsavedRecipes)
+  domUpdates.toggleSelection(".welcome-msg", ".my-recipes-banner")
+}
+
+
+// checkbox search button
+  // const displayFilteredRecipes = () => {
+  //   let search = whateverItIsThatWasSearched
+  //   recipes.methodThatFilters
+  //   domUpdates.displayRecipes()
+  // }
+
+// user favorites button
+  // displayFilteredRecipes by 'favorites'?
+  // dropdown or what display?
+
+// user to cook button
+  // displayFilteredRecipes by 'toCook'?
+
+// user 'cook this' button
+  // method that updates local and posts pantry ingredient changes
+
+//user pantry button
+  // displays ingredients in user pantry
+  // function that updates the pantry locally and does the fetch post
+
+
+
+//     if (selectedIngredients.length > 0) {
+//         this.findRecipesWithCheckedIngredients(selectedIngredients);
+//     }
+
+
+function findCheckedBoxes(checkboxesSelector) {
+  const checkboxes = document.querySelectorAll(checkboxesSelector);
+  const checkboxValues = Array.from(checkboxes)
+  const selectedBoxes = checkboxValues.filter(box => box.checked)
+  findRecipes(selectedBoxes);
+}
+
+function searchAndDisplayRecipesMatchingTagCheckboxes() {
+  findCheckedBoxes(".checked-tag")
+}
+
+
+function searchAndDisplayRecipesForPantryIngredients() {
+  findCheckedBoxes(".pantry-checkbox")
+  this.showAllRecipes();
+}
+
+
+function findRecipesWithCheckedIngredients(selected) {
+  const recipeChecker = (arr, target) => target.every(v => arr.includes(v));
+  const ingredientNames = selected.map(item => {
+      return item.id;
+  })
+  recipes.forEach(recipe => {
+      let allRecipeIngredients = [];
+      recipe.ingredients.forEach(ingredient => {
+          allRecipeIngredients.push(ingredient.name);
+      });
+      if (!recipeChecker(allRecipeIngredients, ingredientNames)) {
+          const domRecipe = document.getElementById(`${recipe.id}`);
+          domRecipe.style.display = "none";
+      }
+  })
+}
+
 // // ADD INGREDIENT FORM
+ // do we need a form here to post ingredients 
+  // so users can update their pantry?
+
+
 // addClickEvent(".add-ingredient-button", addIngredientToPantry)
 // const nameAddedIngredient = document.querySelector(".name-ingredient-form")
 // const quantityAddedIngredient = document.querySelector(".quantity-ingredient-form")
@@ -71,83 +210,3 @@ searchForm.addEventListener("submit", pressEnterSearch);
 //     alert('Sorry, ingredient cannot be added at this time.')
 //   }
 // }
-
-// load
-  // login page html displays
-  // get user data to use for login button search
-  // get all recipes data?
-  // get all ingredients data?
-
-const loadPage = () => {
-  getData('users', users)
-  getData('recipes', recipes)
-  getData('ingredients', ingredients)
-  console.log('hi')
-}
-
-// on login display
-  // create user & display welcome
-  // create & hide display pantry
-  // create & hide display favs 
-  // create & hide display to cook
-
-const returnUserId = () => {
-  const userSearched = users.find(user => user.name === loginInput.value)
-  user = new User(userSearched)
-}
-
-const login = () => {
-  returnUserId()
-  createCards()
-  findTags()
-  findPantryInfo()
-  domUpdates.displayWelcomeBanner()
-}
-const pressEnterSearch = (event) => {
-  event.preventDefault();
-  domUpdates.searchRecipes();
-}
-
-//showWelcomeBanner & showMyRecipesBanner
-domUpdates.toggleSelection(".welcome-msg", ".my-recipes-banner")
-
-//toggleMenu
-const displayMenu = () => {
-  domUpdates.toggleMenu()
-}
-
-const showSavedRecipes = () => {
-  const unsavedRecipes = recipes.filter(recipe => {
-      return !user.favoriteRecipes.includes(recipe.id);
-  });
-  domUpdates.recipeDisplay(unsavedRecipes)
-  domUpdates.toggleSelection(".welcome-msg", ".my-recipes-banner")
-}
-//PLANNING
-
-
-
-// search bar button
-// checkbox search button
-  // const displayFilteredRecipes = () => {
-  //   let search = whateverItIsThatWasSearched
-  //   recipes.methodThatFilters
-  //   domUpdates.displayRecipes()
-  // }
-
-// user favorites button
-  // displayFilteredRecipes by 'favorites'?
-  // dropdown or what display?
-
-// user to cook button
-  // displayFilteredRecipes by 'toCook'?
-
-// user 'cook this' button
-  // method that updates local and posts pantry ingredient changes
-
-//user pantry button
-  // displays ingredients in user pantry
-  // do we need a form here to post ingredients 
-  // so users can update their pantry?
-
-//function that updates the pantry locally and does the fetch post
