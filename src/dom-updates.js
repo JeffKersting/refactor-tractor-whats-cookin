1f71b49713864ddb9d40a761e645c073f24ef399
Allison Dietz <dietz.allison@outlook.com>
2021-01-13 20:05:59 -0700

    Fix tab spacing in dom-Updates to follow linter rules

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,122 +1,122 @@
 const domUpdates = {
-    // CHANGE THE DISPLAY OF ANY ARGS PASSED IN, MUST PASS IN [ARGS]
-    toggle(anyArr) {
-        anyArr.forEach(area =>
-            document.querySelector(area).classList.toggle("hidden"))
-    },
+  // CHANGE THE DISPLAY OF ANY ARGS PASSED IN, MUST PASS IN [ARGS]
+  toggle(anyArr) {
+    anyArr.forEach(area =>
+      document.querySelector(area).classList.toggle("hidden"))
+  },
 
-    // HELPER FUNCTION TO INSERTHTML
-    addDisplay(area, where, what) {
-        document.querySelector(area).insertAdjacentHTML(where, what)
-    },
+  // HELPER FUNCTION TO INSERTHTML
+  addDisplay(area, where, what) {
+    document.querySelector(area).insertAdjacentHTML(where, what)
+  },
 
-    clearDisplay(area) {
-      document.querySelector(area).innerHTML = ''
-    },
+  clearDisplay(area) {
+    document.querySelector(area).innerHTML = ''
+  },
 
-    // CARD DISPLAY
-    displayCards(recipeList) {
-      this.clearDisplay('main')
+  // CARD DISPLAY
+  displayCards(recipeList) {
+    this.clearDisplay('main')
 
-      recipeList.forEach(recipe => {
-        let instructions = '';
-        let ingredients = '';
-        recipe.instructions.forEach(step => {
-            instructions += `<li aria-label="recipe-instruction-step" tabindex="0">${step.instruction}</li><br>`
-        })
-        recipe.ingredients.forEach(ingredient => {
-            ingredients += `<li aria-label="ingredient-required-for-recipe" tabindex="0">${ingredient.name}: ${ingredient.quantity.amount}${ingredient.quantity.unit}</li><br>`
-        })
+    recipeList.forEach(recipe => {
+      let instructions = '';
+      let ingredients = '';
+      recipe.instructions.forEach(step => {
+        instructions += `<li aria-label="recipe-instruction-step" tabindex="0">${step.instruction}</li><br>`
+      })
+      recipe.ingredients.forEach(ingredient => {
+        ingredients += `<li aria-label="ingredient-required-for-recipe" tabindex="0">${ingredient.name}: ${ingredient.quantity.amount}${ingredient.quantity.unit}</li><br>`
+      })
 
-        const shortName = recipe.name.length > 40
+      const shortName = recipe.name.length > 40
         ? recipe.name.substring(0, 40) + "..."
         : recipe.name
 
-        const favoritedClass = recipe.isFavorited ? "favorited" : ''
-        const toCookClass = recipe.isToCook ? "to-cook" : ''
+      const favoritedClass = recipe.isFavorited ? "favorited" : ''
+      const toCookClass = recipe.isToCook ? "to-cook" : ''
 
-        const cardHtml = `<div class="recipe-card ${favoritedClass} ${toCookClass}" id=${recipe.id} name=${recipe.id}>
-          <div class="flip-card">
-            <div class="card-front">
-              <h3 maxlength="40">${shortName}</h3>
-              <div class="card-photo-container">
-                <img src=${recipe.image} class="card-photo-preview" id="img1" alt="${recipe.name} recipe" title="${recipe.name} recipe" aria-label="flip-card-to-reveal-details-for-${recipe.name}" tabindex="0" role="button">
-                <div class="text">
-                  <div id="img2">Click for Instructions</div>
-                </div>
-              </div>
-              <div class="to-cook-button" name=${recipe.id}>
-                  <div id="icon-cook" aria-label="button-to-add-recipe-to-user-list-to-cook" tabindex="0" role="button">🍽</div>
-                  <p id="icon-cook-text">Add to cook</p>
-              </div>
-              <div class="favorite-button" name=${recipe.id}>
-                  <div id="icon-fav" aria-label="button-to-add-recipe-to-user-list-of-favorites" tabindex="0" role="button">&#127822;</div>
-                  <p id="icon-fav-text">Favorite</p>
+      const cardHtml = `<div class="recipe-card ${favoritedClass} ${toCookClass}" id=${recipe.id} name=${recipe.id}>
+        <div class="flip-card">
+          <div class="card-front">
+            <h3 maxlength="40">${shortName}</h3>
+            <div class="card-photo-container">
+              <img src=${recipe.image} class="card-photo-preview" id="img1" alt="${recipe.name} recipe" title="${recipe.name} recipe" aria-label="flip-card-to-reveal-details-for-${recipe.name}" tabindex="0" role="button">
+              <div class="text">
+                <div id="img2">Click for Instructions</div>
               </div>
             </div>
-            <div class="card-back">
-              <div id="exit-recipe" aria-label="button-to-exit-details-view-of-${recipe.name}" tabindex="0" role="button">⤸</div>
-              <p class="instructions-title">${recipe.name}</p>
-              <ol class="ingredients">${ingredients}</ol>
-              <ol class="instructions">${instructions}</ol>
-              <ol class="missing" aria-label="ingredients-user-must-restock-to-cook-${recipe.name}" tabindex="0"></ol>
-              <button class="" type="button" id="compare-recipe" aria-label="button-to-check-user-pantry-stock-of-ingredients-to-cook-${recipe.name}" tabindex="0">Pantry Check?</button>
-              <div id="cooked-recipe">&#10003;</div>
+            <div class="to-cook-button" name=${recipe.id}>
+                <div id="icon-cook" aria-label="button-to-add-recipe-to-user-list-to-cook" tabindex="0" role="button">🍽</div>
+                <p id="icon-cook-text">Add to cook</p>
+            </div>
+            <div class="favorite-button" name=${recipe.id}>
+                <div id="icon-fav" aria-label="button-to-add-recipe-to-user-list-of-favorites" tabindex="0" role="button">&#127822;</div>
+                <p id="icon-fav-text">Favorite</p>
             </div>
           </div>
-        </div>`
+          <div class="card-back">
+            <div id="exit-recipe" aria-label="button-to-exit-details-view-of-${recipe.name}" tabindex="0" role="button">⤸</div>
+            <p class="instructions-title">${recipe.name}</p>
+            <ol class="ingredients">${ingredients}</ol>
+            <ol class="instructions">${instructions}</ol>
+            <ol class="missing" aria-label="ingredients-user-must-restock-to-cook-${recipe.name}" tabindex="0"></ol>
+            <button class="" type="button" id="compare-recipe" aria-label="button-to-check-user-pantry-stock-of-ingredients-to-cook-${recipe.name}" tabindex="0">Pantry Check?</button>
+            <div id="cooked-recipe">&#10003;</div>
+          </div>
+        </div>
+      </div>`
 
-        this.addDisplay("main", "beforeend", cardHtml)
-      })
-    },
+      this.addDisplay("main", "beforeend", cardHtml)
+    })
+  },
 
-    capitalize(words) {
-        return words.split(" ").map(word => {
-          return word.charAt(0).toUpperCase() + word.slice(1);
-        }).join(" ");
-      },
+  capitalize(words) {
+    return words.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  },
 
-    listTags(allTags) {
-        allTags.forEach(tag => {
-          const tagHtml = `<li>
-          <input type="checkbox" class="checked-tag" id="${tag}"
-          aria-label="checkbox-for-${tag}-tag" aria-required="false" tabindex="0">
-          <label for="${tag}">${this.capitalize(tag)}</label>
-          </li>`;
-            this.addDisplay(".tag-list", "beforeend", tagHtml);
-        });
-    },
+  listTags(allTags) {
+    allTags.forEach(tag => {
+      const tagHtml = `<li>
+        <input type="checkbox" class="checked-tag" id="${tag}"
+        aria-label="checkbox-for-${tag}-tag" aria-required="false" tabindex="0">
+        <label for="${tag}">${this.capitalize(tag)}</label>
+        </li>`;
+      this.addDisplay(".tag-list", "beforeend", tagHtml);
+    });
+  },
 
-    showUserPantry(user, ingredients) {
-      const pantryItemArray = Object.keys(user.pantry.pantry)
-      pantryItemArray.forEach(pantryItem => {
-        console.log(user.pantry.pantry[pantryItem])
+  showUserPantry(user, ingredients) {
+    const pantryItemArray = Object.keys(user.pantry.pantry)
+    pantryItemArray.forEach(pantryItem => {
+      console.log(user.pantry.pantry[pantryItem])
 
-        //right now if this is long because we have no names on ingredients
-        // if they added it themselves to the database we have no way of retrieving
-        // that new ingredient name, just an id which I set to Date.now()
-        const ingredientName = ingredients.find(ingredient => ingredient.id == pantryItem)
+      //right now if this is long because we have no names on ingredients
+      // if they added it themselves to the database we have no way of retrieving
+      // that new ingredient name, just an id which I set to Date.now()
+      const ingredientName = ingredients.find(ingredient => ingredient.id == pantryItem)
         ? ingredients.find(ingredient => ingredient.id == pantryItem).name
         : `Item ID: ${user.pantry.pantry[pantryItem].id}`
 
-        const pantryItemHTML = `${user.pantry.pantry[pantryItem].amount} - ${ingredientName}  <br>`
-        console.log(document.querySelector('.pantry-list'))
-        this.addDisplay('.pantry-ingredients', 'beforeend', pantryItemHTML)
-       })
-    },
+      const pantryItemHTML = `${user.pantry.pantry[pantryItem].amount} - ${ingredientName}  <br>`
+      console.log(document.querySelector('.pantry-list'))
+      this.addDisplay('.pantry-ingredients', 'beforeend', pantryItemHTML)
+    })
+  },
 
-    //NOT WORKING
-    showRecipeComparison(missingList) {
-      const missingItemHtml = ["<h2>Missing Ingredients from Your Pantry:</h2>"]
-      missingList.forEach(item => {
-        missingItemHtml.push`<li aria-label="ingredient-to-restock"  tabindex="0">
-        ${item.amount} - ${item.missing}
-        </li>`
-      })
+  //NOT WORKING
+  showRecipeComparison(missingList) {
+    const missingItemHtml = ["<h2>Missing Ingredients from Your Pantry:</h2>"]
+    missingList.forEach(item => {
+      missingItemHtml.push`<li aria-label="ingredient-to-restock"  tabindex="0">
+      ${item.amount} - ${item.missing}
+      </li>`
+    })
 
-      this.displayCards('.missing', 'beforeend', missingItemHtml.join(''))
-    }
-
+    this.displayCards('.missing', 'beforeend', missingItemHtml.join(''))
   }
+
+}
 
 export default domUpdates;
